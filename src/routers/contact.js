099c5d613d23aca46e88d544f10a3038be037d6c
dkaliazin <dkaliazin@gmail.com>
2024-10-12 17:33:32 +0300

    74

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -16,15 +16,14 @@ const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-/*router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));*/
 router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
-/*router.post('/', ctrlWrapper(createContactController));*/
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
-/*router.put('/:contactId',validateBody(createContactSchema), ctrlWrapper(upsertContactController));
-router.patch('/:contactId',validateBody(createContactSchema), ctrlWrapper(patchContactController));*/
 router.put('/:contactId',validateBody(createContactSchema), ctrlWrapper(upsertContactController));
 router.patch('/:contactId',validateBody(createContactSchema), ctrlWrapper(patchContactController));
+router.post(
+  '/register',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-/*router.put('/:contactId',ctrlWrapper(upsertContactController));
-router.patch('/:contactId',ctrlWrapper(patchContactController));*/
 export default router;
