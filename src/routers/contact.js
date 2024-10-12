f2403d207f4674e9353e6a3cfc7b62c1d2ce9f61
dkaliazin <dkaliazin@gmail.com>
2024-10-12 17:27:59 +0300

    1

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -17,10 +17,14 @@ const router = Router();
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 /*router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));*/
-router.post('/', ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+/*router.post('/', ctrlWrapper(createContactController));*/
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 /*router.put('/:contactId',validateBody(createContactSchema), ctrlWrapper(upsertContactController));
 router.patch('/:contactId',validateBody(createContactSchema), ctrlWrapper(patchContactController));*/
-router.put('/:contactId',ctrlWrapper(upsertContactController));
-router.patch('/:contactId',ctrlWrapper(patchContactController));
+router.put('/:contactId',validateBody(createContactSchema), ctrlWrapper(upsertContactController));
+router.patch('/:contactId',validateBody(createContactSchema), ctrlWrapper(patchContactController));
+
+/*router.put('/:contactId',ctrlWrapper(upsertContactController));
+router.patch('/:contactId',ctrlWrapper(patchContactController));*/
 export default router;
