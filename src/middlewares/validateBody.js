f935c55df6a4672cd7cc9f127d50f39b66502f40
dkaliazin <dkaliazin@gmail.com>
2024-10-12 17:15:56 +0300

    validation

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,4 +1,4 @@
-import createHttpError from "http-errors";
+/*import createHttpError from "http-errors";
 import Joi from "joi";
 export const validateBody = (schema) => async (req, res, next) => {
     try {
@@ -11,12 +11,18 @@ export const validateBody = (schema) => async (req, res, next) => {
         })
         next(error);
     }
-}
-export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(30),
-    phoneNumber: Joi.string().email(),
-    email: Joi.string(),
-    isFavorite: Joi.boolean(),
-    contactType:Joi.string().valid('work','home','personal'),
+}*/
+import Joi from 'joi';
 
-})
+export const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        status: 400,
+        message: error.details[0].message,
+      });
+    }
+    next();
+  };
+};
