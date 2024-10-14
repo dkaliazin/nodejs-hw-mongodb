fd9e28dda2231e946f4b15260a07d30af3007b8d
dkaliazin <dkaliazin@gmail.com>
2024-10-14 08:20:03 +0300

    post

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-    name: Joi.string().min(3).optional(),
-    phoneNumber: Joi.string().pattern(/^[0-9\+\-\(\)]+$/).optional(),
+    name: Joi.string().min(3).required(),
+    phoneNumber: Joi.string().pattern(/^[0-9\+\-\(\)]+$/).required(),
     email: Joi.string().email().optional(),
     isFavourite: Joi.boolean().optional(),
     contactType: Joi.string().valid('work', 'home', 'personal').optional()
