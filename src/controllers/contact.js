cda33c73b035246db7d540769fe5089e8d6889ab
dkaliazin <dkaliazin@gmail.com>
2024-10-14 07:57:47 +0300

    post

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -48,9 +48,19 @@ export const getContactByIdController = async (req, res) => {
 };
 //createContact
 export const createContactController = async (req, res) => {
+  const photo = req.file;
+  let photoUrl;
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
   const contactData = {
     ...req.body,
     userId: req.user._id,
+    photo:photoUrl
   };
     const contact = await createContact(contactData);
 
